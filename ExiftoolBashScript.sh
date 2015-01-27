d5a7401b925037134fe21df9af1537c1f830979c
Louise Searle <louise.searle@wur.nl>
2015-01-27 09:13:05 +0100

    Setup ExifTool bash script. To be run in terminal before executing main.

---
+++ ExiftoolBashScript.sh
@@ -0,0 +1,19 @@
+### Louise Searle, January 26 2015
+### Install ExifTool shell script
+
+#!/bin/sh
+echo 'Install ExifTool script'
+echo 'Creating project directories'
+mkdir -p /Users/Louise/Desktop/GeoscriptingLouiseSearle/{photographs,downloads,data,results}
+echo 'Complete'
+echo 'Downloading ExifTool package'
+cd /Users/Louise/Desktop/GeoscriptingLouiseSearle/downloads
+sudo curl -O 'http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-9.82.tar.gz'
+echo 'Complete'
+echo 'Unpacking ExifTool package'
+tar -xzf Image-ExifTool-9.82.tar.gz
+cd Image-ExifTool-9.82
+echo 'Complete'
+echo 'Installing ExifTool package'
+sudo cp -r exiftool lib /usr/bin
+echo 'Script complete'
