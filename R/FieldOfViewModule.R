ef1eb4cd74eaddf6bc6b13d9c2c5c55790ab6146
Louise Searle <louise.searle@wur.nl>
2015-02-02 10:27:42 +0100

    Added documentation and comments.

--- R/FieldOfViewModule.R
+++ R/FieldOfViewModule.R
@@ -1,25 +1,20 @@
-## Louise Searle, January 29 2015.
+### Louise Searle, January 29 2015.
+### Geoscripting Project : Estimating Tree Species Count and Visibility in Photographs using Viewshed Analysis.
 
-## Field of View Module
+### Field of View Module
 
-PointsFOV <- function(photo, distance, angle){
+PointsFOV <- function(photo, distance=150, angle=50){
+  #' Determines the 3 points of a field of view (fov) polygon for a photograph.
+  #' @param photo A point feature of the camera position with coordinates.
+  #' @param distance A numerical value for the maximum distance of the fov.
+  #' @param angle The angle in degrees of the fov.
+  #' @return A data frame containing the 3 points' coordinates.
   # Initiate field of view points coordinates data frame, with photo camera position as first point.
   points_fov <- data.frame('Name' = photo$Name, 
                            'P1X' = photo@coords[,1], 
                            'P1Y' = photo@coords[,2])
-  # Calculate coordinates of points, given photo exif data and field of view angle.
-  points <- PointsPosFOV(photo, angle, distance)
-  # Add points to field of view points data frame.
-  points_fov['P2X'] <- points[1,1]
-  points_fov['P2Y'] <- points[1,2]
-  points_fov['P3X'] <- points[2,1]
-  points_fov['P3Y'] <- points[2,2]
-  # return field of view points data frame.
-  return(points_fov)
-}
-
-PointsPosFOV <- function(photo, fov, distance){
-  angles <- c((photo$Direction-(fov/2)), (photo$Direction+(fov/2)))
+  # Calculate coordinates of 2nd and third points.
+  angles <- c((photo$Direction-(angle/2)), (photo$Direction+(angle/2)))
   points <- matrix(data=NA, ncol=2, nrow=2)
   for(i in 1:2){
     trig_dirx <- ifelse(angles[i]<180, 1, 0) 
@@ -41,15 +36,29 @@ PointsPosFOV <- function(photo, fov, distance){
       points[i,2] <- photo@coords[,2] + offset_y
     }
   }
-  return(points)
+  points_fov['P2X'] <- points[1,1]
+  points_fov['P2Y'] <- points[1,2]
+  points_fov['P3X'] <- points[2,1]
+  points_fov['P3Y'] <- points[2,2]
+  return(points_fov)
 }
 
+
 PolygonFOV <- function(origin, points, project, view_min, view_max){
-  coords_matrix = matrix(c(points[,2], points[,4], points[,6], points[,3], points[,5], points[,7]), nrow=3, ncol=2, byrow=F)
+  #' Creates a field of view (fov) polygon for a photograph.
+  #' @param origin A point feature of the camera position.
+  #' @param points A data frame containing the coordinates for the fov polygon nodes.
+  #' @param project The projection system for the polygon.
+  #' @param view_min The minimum distance of features that the camera can observe.
+  #' @param view_max The maximum distance of features that the camera can observe.
+  #' @return A spatial polygons data frame of the fov for the photograph.
+  # Create triangular polygon from points.
+  coords_matrix = matrix(points[, c(2,4,6,3,5,7)], nrow=3, ncol=2, byrow=F)
   poly <- Polygon(coords_matrix)
   poly_list <- Polygons(list(poly),1)
   poly_sp <- SpatialPolygons(list(poly_list), proj4string=project)
   fov_polygon <- SpatialPolygonsDataFrame(poly_sp, points, match.ID=F)
+  # Subtract area greater than maximum distance and less than minimum distance from camera position.
   max_buffer <- buffer(origin, width=view_max)
   min_buffer <- buffer(origin, width=view_min)
   fov_max <- gIntersection(fov_polygon, max_buffer, byid=T)
--- R/PhotographModule.R
+++ R/PhotographModule.R
@@ -1,11 +1,17 @@
-# PhotographModule
+### Louise Searle, January 29 2015.
+### Geoscripting Project : Estimating Tree Species Count and Visibility in Photographs using Viewshed Analysis.
 
-ExifData <- function(position){  
+### Photograph Module
+
+ExifData <- function(position){
+  #' Extracts the exif data for a photograph required for field of view analysis.
+  #' @param position The position of the photograph, in a list of files for directory of photographs.
+  #' @return A data frame containing the exif data for the photograph.
   # Retrieve exif data from photographs using ExifTool. 
   exif_data <- system('exiftool -T -r -filename -FocalLength -GPSLatitude -GPSLongitude -GPSImgDirection photographs', inter=TRUE)
   exif_data <- gsub('"','', exif_data)
   exif_position <- exif_data[position]
-  # Extract relevant data from exif string with regular expression. FUNCTION! extract fov exif data.
+  # Extract relevant data from exif string with regular expression.
   exif_match <- str_match(exif_position, "(IMG_[0-9]+\\.JPG)\t([0-9]\\.[0-9]) mm\t([0-9][0-9]) deg ([0-9][0-9])\\' ([0-9]+\\.[0-9][0-9]) ([SN])\t([0-9]) deg ([0-9][0-9])\\' ([0-9]+\\.[0-9][0-9]) ([EW])\t([0-9]+)")
   # Store exif data in a dataframe.
   exif.df <- data.frame('Name' = exif_match[,2], 
@@ -15,3 +21,4 @@ ExifData <- function(position){
                         'Longitude' = as.double(exif_match[,8])+(as.double(exif_match[,9])/60)+(as.double(exif_match[,10])/3600))
   return(exif.df)
 }
+
